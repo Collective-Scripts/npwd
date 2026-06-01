25d5ee08382f36e3ed6572301157684ea4c1a183
.clxsx <86518186+clxsx@users.noreply.github.com>
2026-06-01 22:04:59 +0800

    d

--- packages/database/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './darkchat/index';
-export * from './db/index';
--- packages/ui/src/index.ts
+++
@@ -1,8 +0,0 @@
-import './styles.css';
-
-export * from './button/index';
-export * from './input/index';
-export * from './list/index';
-export * from './switch/index';
-export * from './slider/index';
-export * from './utils';
